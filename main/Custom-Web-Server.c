f2a9c504eb276a9a5c854e5333f1ee2aa0c9ca38
jpatrick5402 <jpatrick5402@gmail.com>
2024-11-05 11:30:14 -0500

    Adding comments

--- main/Custom-Web-Server.c
+++ main/Custom-Web-Server.c
@@ -1,3 +1,4 @@
+// webServer & general header files
 #include "string.h"
 #include "stdlib.h"
 #include "unistd.h"
@@ -15,6 +16,7 @@
 #include "esp_check.h"
 #include "sys/socket.h"
 
+// GPIO header files
 #include "inttypes.h"
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
@@ -25,27 +27,25 @@ static const char *TAG = "custom_web_server";
 
 static esp_err_t index_get_handler(httpd_req_t *req)
 {
-    extern const unsigned char index_start[] asm("_binary_index_html_start");
+    extern const unsigned char index_start[] asm("_binary_index_html_start"); // getting html data
     extern const unsigned char index_end[]   asm("_binary_index_html_end");
     const size_t index_size = (index_end - index_start);
 
-    httpd_resp_send(req, (const char *)index_start, index_size); // sending data over
+    httpd_resp_send(req, (const char *)index_start, index_size); // sending html over
 
     // getting client IP address
     int sockfd = httpd_req_to_sockfd(req);
     char ipstr[INET6_ADDRSTRLEN];
     struct sockaddr_in6 addr;
     socklen_t addr_size = sizeof(addr);
-
     if (getpeername(sockfd, (struct sockaddr *)&addr, &addr_size) < 0) {
         ESP_LOGE(TAG, "Error getting client IP");
         return ESP_OK;
     }
-
-    // Convert to IPv4 string
     inet_ntop(AF_INET, &addr.sin6_addr.un.u32_addr[3], ipstr, sizeof(ipstr));
     ESP_LOGI(TAG, "Client IP => %s", ipstr);
 
+    // triggering pin 18 to turn on, wait, and turn off when someone connects
     gpio_set_level(18, 1);
     sleep(1);
     gpio_set_level(18, 0);
@@ -58,10 +58,8 @@ static esp_err_t favicon_get_handler(httpd_req_t *req)
     extern const unsigned char favicon_ico_start[] asm("_binary_favicon_ico_start");
     extern const unsigned char favicon_ico_end[]   asm("_binary_favicon_ico_end");
     const size_t favicon_ico_size = (favicon_ico_end - favicon_ico_start);
-
     httpd_resp_set_type(req, "image/x-icon");
     httpd_resp_send(req, (const char *)favicon_ico_start, favicon_ico_size); // sending data over
-
     return ESP_OK;
 }
 
@@ -83,10 +81,7 @@ static httpd_handle_t start_webserver(void) // starting function for server
 {
     httpd_handle_t server = NULL;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-
     config.lru_purge_enable = true;
-
-    // Start the httpd server
     ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
     if (httpd_start(&server, &config) == ESP_OK) {
         // Set URI handlers
@@ -102,15 +97,13 @@ static httpd_handle_t start_webserver(void) // starting function for server
 }
 
 void app_main(void) { // entry point for our application
+ 
     static httpd_handle_t server = NULL;
-
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect());
-
     server = start_webserver(); // goes to start_webserver() function
-
     ESP_LOGI(TAG, "Waiting for connections");
 
     // GPIO configuration
